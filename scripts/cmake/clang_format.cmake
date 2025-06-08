e68e414b73dc1fea183ed929ad555633d004a6d9
Jamon Bailey <infinity.dev@outlook.com>
2025-06-08 18:24:29 -0400

    Create clang_format.cmake

---
+++ scripts/cmake/clang_format.cmake
@@ -0,0 +1,44 @@
+
+# Search directories recursively for formattable source files
+macro(format_source_code)
+    set(
+        FORMAT_SEARCH_DIRS
+        "${CMAKE_SOURCE_DIR}/libs"
+        "${CMAKE_SOURCE_DIR}/include"
+        "${CMAKE_SOURCE_DIR}/src"
+        "${CMAKE_SOURCE_DIR}/tests"
+    )
+
+    set(
+        SOURCE_EXTENSIONS
+        *.c
+        *.cpp
+        *.h
+        *.hpp
+    )
+
+    set(SOURCE_FILE_LIST "")
+
+    foreach(DIR ${FORMAT_SEARCH_DIRS})
+        foreach(EXTENSION ${SOURCE_EXTENSIONS})
+            file(
+                GLOB_RECURSE
+                tmp_files
+
+                CONFIGURE_DEPENDS
+                "${DIR}/${EXTENSION}"
+            )
+            list(APPEND SOURCE_FILE_LIST ${tmp_files})
+        endforeach()
+    endforeach()
+
+    message(STATUS "Formatting source files...")
+
+    add_custom_target(
+        code_format
+
+        COMMAND clang-format -i ${SOURCE_FILE_LIST}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMENT "Running clang-format on source files..."
+    )
+endmacro()
