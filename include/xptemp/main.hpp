b72556aee66b2c4acb80debe803b3550e2207074
Jamon Bailey <infinity.dev@outlook.com>
2025-06-09 13:40:23 -0400

    Create Example main.hpp Header

---
+++ include/xptemp/main.hpp
@@ -0,0 +1,11 @@
+
+#ifndef XPTEMP_LIB_SAMPLE_SOURCE_H_
+#define XPTEMP_LIB_SAMPLE_SOURCE_H_
+
+/*!
+* @brief
+* Returns the sum of val1 and val2.
+*/
+int myAdd(const int val1, const int val2);
+
+#endif // XPTEMP_LIB_SAMPLE_SOURCE_H_
