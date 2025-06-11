e207ab3dc23b0f5c070d204657960b1681a6bd20
Jamon Bailey <infinity.dev@outlook.com>
2025-06-11 17:34:55 -0400

    Converted Demo Target to Executable
    Converted demonstration target to an executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 
+#include<iostream>
 #include"xptemp/main.hpp"
+
+int myAdd(const int val1, const int val2);
+
+int main(const int argc, char* argv[]) {
+    const int value1 = 1;
+    const int value2 = 9;
+
+    std::cout << "\nThe sum of " << value1 << " and " << value2 << " is:";
+
+    const int sum = myAdd(value1, value2);
+
+    std::cout << '\n' << sum << std::endl;
+
+    return 0;
+}
 
 int myAdd(const int val1, const int val2) {
     return val1+val2;
