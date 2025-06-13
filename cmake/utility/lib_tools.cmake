e8f0dd66162a9b94a2fab2966f7e99a1e0ce2149
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2025-06-13 15:51:36 -0400

    Update lib_tools.cmake
    Updated CMake module; external library target macro

--- cmake/utility/lib_tools.cmake
+++ cmake/utility/lib_tools.cmake
@@ -1,10 +1,10 @@
 
-# Add precompiled static or shared library target (shared libs: Linux and macOS)
+# Add precompiled static or shared library target
 macro(add_external_library TARGET_NAME LIB_TYPE LIB_PATH)
     cmake_parse_arguments(
         ARGSS
         ""
-        ""
+        "IMPORT_LIB"
         "INCLUDE"
         ${ARGN}
     )
@@ -14,23 +14,75 @@ macro(add_external_library TARGET_NAME LIB_TYPE LIB_PATH)
     add_library(${TARGET_NAME} ${LIB_TYPE} IMPORTED)
 
     if(NOT ARGSS_INCLUDE)
-        set_target_properties(
-            ${TARGET_NAME}
-            PROPERTIES
+        if(NOT ARGSS_IMPORT_LIB)
+            set_target_properties(
+                ${TARGET_NAME}
+                PROPERTIES
 
-            IMPORTED_LOCATION
-            "${LIB_PATH}"
-        )
+                IMPORTED_LOCATION
+                "${LIB_PATH}"
+            )
+        else()
+            set_target_properties(
+                ${TARGET_NAME}
+                PROPERTIES
+
+                IMPORTED_IMPLIB
+                "${ARGSS_IMPORT_LIB}"
+
+                IMPORTED_LOCATION
+                "${LIB_PATH}"
+            )
+        endif()
     else()
-        set_target_properties(
-            ${TARGET_NAME}
-            PROPERTIES
+        if(NOT ARGSS_IMPORT_LIB)
+            set_target_properties(
+                ${TARGET_NAME}
+                PROPERTIES
 
-            IMPORTED_LOCATION
-            "${LIB_PATH}"
+                IMPORTED_LOCATION
+                "${LIB_PATH}"
 
-            INTERFACE_INCLUDE_DIRECTORIES
-            ${ARGSS_INCLUDE}
-        )
+                INTERFACE_INCLUDE_DIRECTORIES
+                "${ARGSS_INCLUDE}"
+            )
+        else()
+            set_target_properties(
+                ${TARGET_NAME}
+                PROPERTIES
+
+                IMPORTED_IMPLIB
+                "${ARGSS_IMPORT_LIB}"
+
+                IMPORTED_LOCATION
+                "${LIB_PATH}"
+
+                INTERFACE_INCLUDE_DIRECTORIES
+                "${ARGSS_INCLUDE}"
+            )
+        endif()
     endif()
 endmacro()
+
+# If Linux/macOS adding shared library:
+# add_external_library(
+#     TestLib
+#     SHARED
+#     "./lib/testlib/libtest.so"
+# 
+#     INCLUDE
+#     ${PRJ_INCLUDE_DIRS}
+# )
+
+# If Windows adding shared library:
+# add_external_library(
+#     TestLib
+#     SHARED
+#     "./lib/testlib/test.dll"
+# 
+#     IMPORT_LIB
+#     "./lib/testlib/test.lib"
+# 
+#     INCLUDE
+#     ${PRJ_INCLUDE_DIRS}
+# )
