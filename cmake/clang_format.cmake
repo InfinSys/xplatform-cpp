75e768d7fbf2c6471113fc1e9331ef188cfb4cce
Jamon Bailey <infinity.dev@outlook.com>
2025-06-15 00:49:11 -0400

    Clang-Format Module Update

--- cmake/clang_format.cmake
+++ cmake/clang_format.cmake
@@ -3,47 +3,40 @@
 #     CODEBASE FORMATTING MODULE (Clang-Format)
 #====================================================
 
-# Search directories recursively for formattable source files
-macro(format_source_code)
-    # TODO: Configure which directories clang-format visits recursively...
-    set(
-        FORMAT_SEARCH_DIRS
-        "${CMAKE_SOURCE_DIR}/lib"
-        "${CMAKE_SOURCE_DIR}/include"
-        "${CMAKE_SOURCE_DIR}/src"
-        "${CMAKE_SOURCE_DIR}/tests"
-    )
+# TODO: Configure which directories clang-format visits recursively...
+set(
+    FORMAT_SEARCH_DIRS
+    "${CMAKE_SOURCE_DIR}/lib"
+    "${CMAKE_SOURCE_DIR}/include"
+    "${CMAKE_SOURCE_DIR}/src"
+    "${CMAKE_SOURCE_DIR}/tests"
+)
 
-    set(
-        SOURCE_EXTENSIONS
-        *.c
-        *.cpp
-        *.h
-        *.hpp
-    )
+set(
+    SOURCE_EXTENSIONS
+    *.c
+    *.cpp
+    *.h
+    *.hpp
+)
 
-    set(SOURCE_FILE_LIST "")
+set(SOURCE_FILE_LIST "")
 
-    foreach(DIR ${FORMAT_SEARCH_DIRS})
-        foreach(EXTENSION ${SOURCE_EXTENSIONS})
-            file(
-                GLOB_RECURSE
-                tmp_files
-
-                CONFIGURE_DEPENDS
-                "${DIR}/${EXTENSION}"
-            )
-            list(APPEND SOURCE_FILE_LIST ${tmp_files})
-        endforeach()
+foreach(DIR ${FORMAT_SEARCH_DIRS})
+    foreach(EXTENSION ${SOURCE_EXTENSIONS})
+        file(
+            GLOB_RECURSE
+            tmp_files
+            CONFIGURE_DEPENDS
+            "${DIR}/${EXTENSION}"
+        )
+        list(APPEND SOURCE_FILE_LIST ${tmp_files})
     endforeach()
+endforeach()
 
-    message(STATUS "Formatting source files...")
-
-    add_custom_target(
-        code_format
-
-        COMMAND clang-format -i ${SOURCE_FILE_LIST}
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        COMMENT "Formatting source files..."
-    )
-endmacro()
+add_custom_target(
+    code_format
+    COMMAND clang-format -i ${SOURCE_FILE_LIST}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Formatting source files..."
+)
