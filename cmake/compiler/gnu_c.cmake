83bc4cafad3a59896d56266541f5c19de7883495
Jamon Bailey <infinity.dev@outlook.com>
2025-10-04 22:05:11 -0400

    Updated GNU Compiler Config Scripts
    Following a "modern CMake" philosophy where configurations are made on an individual target basis

--- cmake/compiler/gnu_c.cmake
+++ cmake/compiler/gnu_c.cmake
@@ -5,34 +5,99 @@
 
 # TODO: Setup gcc compiler configuration (if applicable)
 
-set(
-    C_COMPILE_FLAGS
-    ""
+set(${PRJ_PREFIX}_GCC_C_VERSION_MIN 14 CACHE STRING "Minimum gcc compiler version")
+
+# Confirm supported gcc compiler version
+if(CMAKE_C_COMPILER_VERSION VERSION_LESS ${${PRJ_PREFIX}_GCC_C_VERSION_MIN})
+    message(FATAL_ERROR "Incompatible version of GNU C compiler for ${CMAKE_PROJECT_NAME}.")
+endif()
+
+# Compiler feature configuration target
+add_library(${PRJ_PREFIX}_gcc_c_features INTERFACE)
+# Compiler diagnostics configuration target
+add_library(${PRJ_PREFIX}_gcc_c_warnings INTERFACE)
+# General compiler configuration target
+add_library(${PRJ_PREFIX}_gcc_c_options INTERFACE)
+# Preprocessor definitions target
+add_library(${PRJ_PREFIX}_gcc_c_defines INTERFACE)
+
+# Set C standard version
+target_compile_features(
+    ${PRJ_PREFIX}_gcc_c_features
+
+    INTERFACE
+        c_std_11
 )
 
-set(
-    C_WARNING_FLAGS
-    ""
-)
-
-set(
-    C_PP_DEFINITIONS
-    ""
-)
-
-add_library(GLOBAL_C_OPTIONS INTERFACE)
-
+# Set compiler warning flags
 target_compile_options(
-    GLOBAL_C_OPTIONS
+    ${PRJ_PREFIX}_gcc_c_warnings
 
     INTERFACE
-    ${C_COMPILE_FLAGS}
-    ${C_WARNING_FLAGS}
+        # Compiler warning flags
+        "-Wall"
+        "-Wextra"
+        "-Wpedantic"
 )
 
-target_compile_definitions(
-    GLOBAL_C_OPTIONS
+# Conditionally add more aggressive warnings
+if(${PRJ_PREFIX}_STRICT_C_WARNINGS)
+    target_compile_options(
+        ${PRJ_PREFIX}_gcc_c_warnings
+
+        INTERFACE
+            "-Werror"
+            "-Wshadow"
+            "-Wdouble-promotion"
+            "-Wformat=2"
+    )
+endif()
+
+# Conditionally set compiler optimization level
+target_compile_options(
+    ${PRJ_PREFIX}_gcc_c_options
 
     INTERFACE
-    ${C_PP_DEFINITIONS}
+        # Disable optimization on debug
+        $<$<CONFIG:Debug>:
+            "-O0"
+        >
+
+        # High optimization on release
+        $<$<CONFIG:Release>:
+            "-O2"
+        >
+)
+
+# Define preprocessor definitions
+target_compile_definitions(
+    ${PRJ_PREFIX}_gcc_c_defines
+
+    INTERFACE
+        # Unconditional preprocessor definitions
+        ${PRJ_PREFIX}_C_LANG
+
+        # Preprocessor definitions on debug
+        $<$<CONFIG:Debug>:
+            _${PRJ_PREFIX}_C_DEBUG
+            ${PRJ_PREFIX}_C_DEBUG
+        >
+
+        # Preprocessor definitions on release
+        $<$<CONFIG:Release>:
+            ${PRJ_PREFIX}_C_RELEASE
+        >
+)
+
+# Complete GNU C compiler package
+add_library(${PRJ_PREFIX}_gcc_c_bundle INTERFACE)
+
+target_link_libraries(
+    ${PRJ_PREFIX}_gcc_c_bundle
+    
+    INTERFACE
+        ${PRJ_PREFIX}_gcc_c_features
+        ${PRJ_PREFIX}_gcc_c_warnings
+        ${PRJ_PREFIX}_gcc_c_options
+        ${PRJ_PREFIX}_gcc_c_defines
 )
