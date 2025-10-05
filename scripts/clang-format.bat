896ed4215de688361de4854205e3b591750aa42c
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2025-10-04 20:43:33 -0400

    Delete scripts directory

--- scripts/clang-format.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd %~dp0/..
-cmake --build --preset win32-x64-Debug --target code_format
-
-echo Done.
-echo.
-pause
--- scripts/clang-format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-cmake --build --preset linux-x64-Debug --target code_format
-
-echo "Done."
-echo ""
-read -p "Press Enter to continue..."
