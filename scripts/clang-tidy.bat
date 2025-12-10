9ea4858ad3bf2423b9407c1d62dc91b8f1ed82ce
Jamon Bailey <infinity.dev@outlook.com>
2025-12-10 03:28:26 -0500

    Updated Project Scripts
    New scripts for Clang-Tidy; Updates made to clang-format.bat and clang-format.sh to allow variable preset names

--- scripts/clang-tidy.bat
+++ scripts/clang-tidy.bat
@@ -6,9 +6,10 @@ IF "%~1"=="" GOTO :usage
 
 SET "CMAKE_PRESET=%~1"
 
+:: Start...
 cd %~dp0/..
-cmake --build --preset "%CMAKE_PRESET%" --target XPTEMP_clang_format
-:: TODO: Change 'XPTEMP_clang_format' to my Clang-Format utility target name
+cmake --build --preset "%CMAKE_PRESET%" --target XPTEMP_clang_tidy
+:: TODO: Change 'XPTEMP_clang_tidy' to my Clang-Tidy utility target name
 
 echo Done.
 echo.
