ccd9991c6cb7e17d74e5c4027b885852a7de2dfc
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2026-06-25 23:11:31 -0400

    C++ Compiler Config Module Updates
    - Updated all C++ compiler configurations scripts to feature documentation and renamed some references to the variable `${PRJ_PREFIX}` to `${PRJ_PREFIX_L}` for lowercase version of project prefix
    
    - Cosmetic adjustments

--- cmake/compiler/gcc_cxx.cmake
+++ cmake/compiler/gcc_cxx.cmake
@@ -3,101 +3,119 @@
 #     GCC C++ COMPILER CONFIGURATION (g++)
 #===============================================
 
+include_guard(GLOBAL)
+
+# --------------------------------------------------------------------
+# @file
+# cmake/compiler/gcc_cxx.cmake
+#
+# @note
+# Intended to be included from the root `CMakeLists.txt` script,
+# generally after the `cmake/compiler/global_cxx.cmake` module has
+# made the global C++ compiler configuration available as a named
+# target. The inclusion of the global C++ configuration and this
+# module can be done manually, or you can simply include the
+# `cmake/compiler/config_dispatch.cmake` module to handle both in
+# a single step. It is advised to do the latter for portability.
+#
+# @details
+# Assembles the complete g++ compiler configuration bundle by defining
+# focused interface targets covering C++ standard enforcement, warning
+# policy, optimization strategy, and GCC-specific preprocessor symbols.
+# These individual targets, together with the global C++ configuration,
+# are consolidated into a single linkable target. This module also
+# enforces a minimum compiler version upfront to prevent the use of an
+# unsupported toolchain.
+#
+# @author Jamon T. Bailey
+# @date 06-09-2025
+# --------------------------------------------------------------------
+
 # TODO: Setup g++ compiler configuration (if applicable)
 
 set(${PRJ_PREFIX}_GCC_CXX_VERSION_MIN 14 CACHE STRING "Minimum g++ compiler version")
 
 # Confirm supported g++ compiler version
 if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${${PRJ_PREFIX}_GCC_CXX_VERSION_MIN})
-    message(FATAL_ERROR "Incompatible version of GCC C++ compiler for ${CMAKE_PROJECT_NAME}.")
+    message(FATAL_ERROR "Incompatible version of g++ compiler for ${CMAKE_PROJECT_NAME}.")
 endif()
 
 # Compiler feature configuration target
-add_library(${PRJ_PREFIX}_gcc_cxx_features INTERFACE)
+add_library(${PRJ_PREFIX_L}_gcc_cxx_features INTERFACE)
 # Compiler diagnostics configuration target
-add_library(${PRJ_PREFIX}_gcc_cxx_warnings INTERFACE)
+add_library(${PRJ_PREFIX_L}_gcc_cxx_warnings INTERFACE)
 # General compiler configuration target
-add_library(${PRJ_PREFIX}_gcc_cxx_options INTERFACE)
+add_library(${PRJ_PREFIX_L}_gcc_cxx_options INTERFACE)
 # Preprocessor definitions target
-add_library(${PRJ_PREFIX}_gcc_cxx_defines INTERFACE)
+add_library(${PRJ_PREFIX_L}_gcc_cxx_defines INTERFACE)
 
 # Set C++ standard version
 target_compile_features(
-    ${PRJ_PREFIX}_gcc_cxx_features
+    ${PRJ_PREFIX_L}_gcc_cxx_features
 
     INTERFACE
-        cxx_std_20
+      cxx_std_20
 )
 
 # Set compiler warning flags
 target_compile_options(
-    ${PRJ_PREFIX}_gcc_cxx_warnings
+    ${PRJ_PREFIX_L}_gcc_cxx_warnings
 
     INTERFACE
-        # Compiler warning flags
-        "-Wall"
-        "-Wextra"
-        "-Wpedantic"
+      # Compiler warning flags
+      -Wall
+      -Wextra
+      -Wpedantic
 )
 
 # Conditionally add more aggressive warnings
 if(${PRJ_PREFIX}_STRICT_CXX_WARNINGS)
     target_compile_options(
-        ${PRJ_PREFIX}_gcc_cxx_warnings
+        ${PRJ_PREFIX_L}_gcc_cxx_warnings
 
         INTERFACE
-            "-Werror"
-            "-Wshadow"
-            "-Wdouble-promotion"
-            "-Wformat=2"
+          -Werror
+          -Wshadow
+          -Wdouble-promotion
+          -Wformat=2
     )
 endif()
 
 # Conditionally set compiler optimization level
 target_compile_options(
-    ${PRJ_PREFIX}_gcc_cxx_options
+    ${PRJ_PREFIX_L}_gcc_cxx_options
 
     INTERFACE
-        # Disable optimization on debug
-        $<$<CONFIG:Debug>:
-            "-O0"
-        >
+      # Disable optimization on debug
+      $<$<CONFIG:Debug>:
+          -O0
+      >
 
-        # High optimization on release
-        $<$<CONFIG:Release>:
-            "-O2"
-        >
+      # High optimization on release
+      $<$<CONFIG:Release>:
+          -O2
+      >
 )
 
 # Define preprocessor definitions
 target_compile_definitions(
-    ${PRJ_PREFIX}_gcc_cxx_defines
+    ${PRJ_PREFIX_L}_gcc_cxx_defines
 
     INTERFACE
-        # Unconditional preprocessor definitions
-        ${PRJ_PREFIX}
-
-        # Preprocessor definitions on debug
-        $<$<CONFIG:Debug>:
-            _${PRJ_PREFIX}_DEBUG
-            ${PRJ_PREFIX}_DEBUG
-        >
-
-        # Preprocessor definitions on release
-        $<$<CONFIG:Release>:
-            ${PRJ_PREFIX}_RELEASE
-        >
+      # Unconditional preprocessor definitions
+      ${PRJ_PREFIX}_GCC
 )
 
 # Complete GCC C++ compiler package
-add_library(${PRJ_PREFIX}_gcc_cxx_bundle INTERFACE)
+add_library(${PRJ_PREFIX_L}_gcc_cxx_bundle INTERFACE)
 
 target_link_libraries(
-    ${PRJ_PREFIX}_gcc_cxx_bundle
+    ${PRJ_PREFIX_L}_gcc_cxx_bundle
     
     INTERFACE
-        ${PRJ_PREFIX}_gcc_cxx_features
-        ${PRJ_PREFIX}_gcc_cxx_warnings
-        ${PRJ_PREFIX}_gcc_cxx_options
-        ${PRJ_PREFIX}_gcc_cxx_defines
+      ${PRJ_PREFIX_L}_gcc_cxx_features
+      ${PRJ_PREFIX_L}_gcc_cxx_warnings
+      ${PRJ_PREFIX_L}_gcc_cxx_options
+      ${PRJ_PREFIX_L}_gcc_cxx_defines
+      ${PRJ_PREFIX_L}_global_cxx
 )
